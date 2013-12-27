5c4b94977f2c23f0f045864f32d8064f38470e25
Bruno Ohana <bohana@gmail.com>
2013-12-27 17:28:04 +0000

    stopwords - 1st commit

---
+++ sentlex/stopwords.py
@@ -0,0 +1,31 @@
+'''
+  Stopwords class - part of sentlex
+
+  This class responds to True/False queries about whether a word is a stop word, based on a source file.
+  The default stop words file is derived from Cornell Univ. SMART information system, and van Rijsbergen:
+  http://nlp.uned.es/~ircourse/examples/stoplist.html
+  http://www.lextek.com/manuals/onix/stopwords2.html
+'''
+
+import os
+
+
+class Stopword(object):
+    '''
+     Stopword class - encapsulates dict containing all known stop words in lowercase
+    '''
+
+    def __init__(self, filename=None):
+        self.worddict = {}
+        if not filename:
+            curpath = os.path.dirname(os.path.abspath(__file__))
+            filename = os.path.join(curpath, '../data/objective.txt')
+            self.load(filename)
+
+    def load(self, filename):
+        f = open(filename)
+        for word in f.readlines():
+            self.worddict[word[:-1].lower()] = 1
+
+    def is_stop(self, word):
+        return self.worddict.has_key(word.lower())
---
+++ setup.py
@@ -0,0 +1,21 @@
+'''
+  setup.py for sentlex
+'''
+from distutils.core import setup
+
+setup(
+    name='SentLex',
+    version='0.1.1dev',
+    author='Bruno Ohana',
+    author_email='bohana@gmail.com',
+    packages=['sentlex', 'sentlex.tests'],
+    package_data={'sentlex': ['data/*.dat', 'data/*.lex', 'data/*.txt']},
+    scripts=['bin/sentutil'],
+    url='https://github.com/bohana/sentlex',
+    license='LICENSE',
+    description='Tools and library for lexicon-based sentiment analysis.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "nltk >= 2.0.4"
+    ],
+)
