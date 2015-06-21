e5ca5403e3cabc7c063f61177f86ef5b28073cc0
Bruno Ohana <bohana@gmail.com>
2015-06-21 19:58:44 +0100

    sentence classifier

--- sentlex/sentanalysis_sent.py
+++ sentlex/sentanalysis_sent.py
@@ -5,8 +5,6 @@
    sentanalysis_sent.py - a sentence-based sentiment classifier
 
 '''
-
-# library imports
 import sentlex
 import collections
 from docscoreutil import *
@@ -31,36 +29,39 @@ class SentenceDocSentiScore(DocSentiScore):
             self.sentence_classifier.set_lexicon(Lex)
 
         self.set_lexicon(Lex)
+        self.question_neg_weight = 0.0
+        self.min_question_size = 0.0       
+        self.verbose = False
 
     def _sent_tokenize(self, doc, separator):
         '''
          Takes a pos-tagged doc and tag separator as input and return list of strings
          each containing a sentence from the original document.
         '''
-        # list of all POS-tagged indicators of end of sentence
-        end_of_sentence = [x+separator+'.' for x in ['.','!','?']]
+        end_of_sentence = [x + separator + '.' for x in ('.', '!', '?')]
+        
         cur_sent = []
         sentences = []
         for token in doc.split(' '):
             cur_sent.append(token)
             if token in end_of_sentence:
-                sentences.append(' '.join(cur_sent))
+                sentences.append((' '.join(cur_sent), len(cur_sent)))
                 cur_sent = []
+
         # finally, if cur_sent is not empty count as last sentence
         if cur_sent:
-            sentences.append(' '.join(cur_sent))
+            sentences.append((' '.join(cur_sent), len(cur_sent)))
+
         return sentences
 
-    def _calc_sentence_scores(self, sent_scores):
-        '''
-         Given list of tuples indicating individual scores for each sentence, returns aggregate (pos,neg) scores for document.
-         The default method simply returns sum of scores for positive and negative results.
-        '''
-        pos_total = sum([x[0] for x in sent_scores])
-        neg_total = sum([x[1] for x in sent_scores])
-        return (pos_total, neg_total)
+    def _is_question(self, sentence):
+        if len(sentence) > 0:
+            # looking for 1st char of the last pos-tagged token in sentence, format "char/POS"
+            if len(sentence[-1]) > 0:
+                return (sentence[-1][0] == '?')
+        return False
 
-    def classify_document(self, Doc, tagged=True, verbose=True, **kwargs):
+    def classify_document(self, Doc, tagged=True, verbose=False, **kwargs):
         '''
           Tokenize input document into sentences, then call classifier on each sentence separately.
           Return aggregate positive and negative scores in tuple (postotal,negtotal)
@@ -80,8 +81,8 @@ class SentenceDocSentiScore(DocSentiScore):
 
         # tokenize into sentences
         tagged_sentences = self._sent_tokenize(tagged_doc, tagsep)
+
         self._debug('[sent classifier] - Found %d sentences' % len(tagged_sentences))
-        sent_scores = []
         # initialize data structure containing results
         self.resultdata = {
             'annotated_doc': '',
@@ -93,18 +94,20 @@ class SentenceDocSentiScore(DocSentiScore):
             'found_list': collections.Counter(),
             'unscored_list': []
         }
-        for sentence in tagged_sentences:
-            # classify sentence
+
+        (tot_pos, tot_neg) = (0.0, 0.0)
+        for (sentence, sent_sz) in tagged_sentences:
             try:
                 self._debug('[sent classifier] %s' % sentence)
-                (cur_pos, cur_neg)=self.sentence_classifier.classify_document(sentence, tagged=True, verbose=verbose)
-                if cur_pos>cur_neg:
-                   sent_scores.append((1,0))
-                elif cur_neg>cur_pos:
-                   sent_scores.append((0,1))
-                else:
-                   sent_scores.append((0,0))
-                self._debug('[sent classifier] - sentence scores: %s' % str(sent_scores))
+                (cur_pos, cur_neg) = self.sentence_classifier.classify_document(sentence, tagged=True, verbose=verbose)
+
+                # adjust for question
+                if self._is_question(sentence.split(' ')) and sent_sz > self.min_question_size:
+                    self._debug('[sent classifier] applying adjustment of %.2f to question sentence' % self.question_neg_weight)
+                    cur_neg += self.question_neg_weight
+
+                tot_pos += cur_pos
+                tot_neg += cur_neg
 
                 # update algorithm results
                 self.resultdata['tokens_found'] += self.sentence_classifier.resultdata['tokens_found']
@@ -112,18 +115,21 @@ class SentenceDocSentiScore(DocSentiScore):
                 self.resultdata['tokens_negated'] += self.sentence_classifier.resultdata['tokens_negated']
                 self.resultdata['unscored_list'] += self.sentence_classifier.resultdata['unscored_list']
                 self.resultdata['found_list'].update(self.sentence_classifier.resultdata['found_list'])
-            except Exception,e:
-                self._debug('[sent classifier] - Error processing sentence: %s' % str(e))
-                raise #continue
+            except Exception, e:
+                print '[sent classifier] - Error processing sentence (%s): %s' % (sentence, str(e))
+                raise
 
-        (resultpos, resultneg) = self._calc_sentence_scores(sent_scores)
-        self.resultdata['resultpos'] = resultpos
-        self.resultdata['resultneg'] = resultneg
-        self.resultdata['sentence_scores'] = sent_scores
-        return (resultpos, resultneg)
+        self.resultdata['resultpos'] = tot_pos
+        self.resultdata['resultneg'] = tot_neg
+        return (tot_pos, tot_neg)
 
     def set_parameters(self, **kwargs):
         '''
          Algorithm parameters are set for the underlying sentence classifier algorithm.
         '''
+        if 'question_minsize' in kwargs:
+            self.min_question_size = kwargs['question_minsize']
+        if 'question_neg_weight' in kwargs:
+            self.question_neg_weight = kwargs['question_neg_weight']
+
         self.sentence_classifier.set_parameters(**kwargs)
